27d14e7e0de673304c1079a2b4f081f6e0e19dec
vircharat <112969307+vircharat@users.noreply.github.com>
2022-09-20 17:51:22 +0530

    Add files via upload

---
+++ Library_management/Library_management/UserPL.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_management
+{
+    public class UserPL
+    {
+        public string user;
+
+        public void AddUser()
+        {
+
+        }
+        public void DeleteReceive()
+        {
+
+        }
+        public void GetAllUser()
+        {
+
+        }
+        public void GetReceivedBook()
+        {
+
+        }
+        public void GetRequestBook()
+        {
+
+        }
+
+        public void GetUserHomeMenu()
+        {
+
+
+
+        }
+
+        public void GetUserMenu()
+        {
+
+        }
+
+        public void GetUserReceivedBook()
+        {
+
+        }
+        public void GetUserRequestBook()
+        {
+
+        }
+
+        public void ReceiveSection()
+        {
+
+        }
+        public void RemoveUser()
+        {
+
+        }
+
+        public void RequestBook()
+        {
+
+        }
+
+        public void RequestSection()
+        {
+
+        }
+        
+        public void UpdateUser()
+        {
+
+        }
+        public void UserHomeSection()
+        {
+
+        }
+
+        public void UserLogin()
+        {
+
+        }
+
+        public void UserSection()
+        {
+            
+        }
+
+
+
+
+
+    }
+}
